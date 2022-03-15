17d5fa765544e39b0729d2b9b4b29b09c128012c
Ilya <alexeenkov-frontend@yandex.ru>
2022-03-15 21:51:07 +0300

    Добавил экспериментальный тест

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,15 +2,13 @@ import Game from "../Game";
 
 const app = new Game(4);
 
-// test("Метод init() вызывает методы generateBoard, insertBoardOnPage и showRandomGoblins", () => {
-//   app.generateBoard = jest.fn();
-//   app.insertBoardOnPage = jest.fn();
-//   app.showRandomGoblins = jest.fn();
-//   app.init();
-//   expect(app.generateBoard).toBeCalled();
-//   expect(app.insertBoardOnPage).toBeCalled();
-//   expect(app.showRandomGoblins).toBeCalled();
-// });
+test("Метод init() вызывает методы generateBoard, insertBoardOnPage", () => {
+  app.generateBoard = jest.fn();
+  app.insertBoardOnPage = jest.fn();
+  app.init();
+  expect(app.generateBoard).toBeCalled();
+  expect(app.insertBoardOnPage).toBeCalled();
+});
 
 // test("Метод generateRandomCell возвращает случайное число от 0 до 15 включительно (когда клеток в таблице 16)", () => {
 //   expect(app.generateRandomCell(4)).toBeGreaterThan(0);
